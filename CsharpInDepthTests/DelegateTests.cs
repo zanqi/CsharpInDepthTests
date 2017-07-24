2afda38890e1bed924e616607ddf8c3bed569969
Zanqi Liang <Zanqi_Liang@epam.com>
2017-07-24 16:07:52 +0800

    Delegate variance

--- CsharpInDepthTests/DelegateTests.cs
+++ CsharpInDepthTests/DelegateTests.cs
@@ -14,7 +14,7 @@ namespace CsharpInDepthTests
     [TestClass]
     public class DelegateTests
     {
-        delegate void MyDelegate(string input);
+        delegate object MyDelegate(string input);
 
         event MyDelegate MyEvent;
 
@@ -29,10 +29,33 @@ namespace CsharpInDepthTests
             // Compile error:
             // Error CS0070  The event 'MyClass.MyEvent' can only appear on the left hand side of += or -= (except when used from within the type 
 
-
             // Inside a class, you can invoke the delegate
-            MyEvent += (s) => { Debug.WriteLine("My Event is invoked from inside!"); };
+            MyEvent += (s) => { return new object(); };
             MyEvent.Invoke("");
+        }
+
+        object MyMethod(object o)
+        {
+            return new object();
+        }
+
+        string MyMethod2(string s)
+        {
+            return s;
+        }
+
+        [TestMethod]
+        public void DelegateVarianceIsSupported()
+        {
+            // Param type Contravariance supported
+            MyEvent += MyMethod;
+
+            Action<object> myAction = (o) => { };
+            //MyEvent += myAction;
+            // This is not: Cannot implicitly convert type 'System.Action<object>' to 'CsharpInDepthTests.DelegateTests.MyDelegate' 
+
+            // Return type Covariance supported
+            MyEvent += MyMethod2;
         }
     }
 }
