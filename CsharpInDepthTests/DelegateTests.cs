62e8b5b91bf54d32e891c656c088e3ff557e1adc
Zanqi Liang <Zanqi_Liang@epam.com>
2017-07-24 15:45:53 +0800

    Delegate test

---
+++ CsharpInDepthTests/DelegateTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace CsharpInDepthTests
+{
+    public class MyClass
+    {
+        public delegate void MyDelegate(string input);
+
+        public event MyDelegate MyEvent;
+    }
+
+    [TestClass]
+    public class DelegateTests
+    {
+        delegate void MyDelegate(string input);
+
+        event MyDelegate MyEvent;
+
+        [TestMethod]
+        public void EventIsEncapsulatingDelegate()
+        {
+            // Outside of a class, you can only + and - on the event
+            var obj = new MyClass();
+            obj.MyEvent += (s) => { Debug.WriteLine("My Event is invoked from outside!"); };
+
+            // obj.MyEvent.Invoke("");
+            // Compile error:
+            // Error CS0070  The event 'MyClass.MyEvent' can only appear on the left hand side of += or -= (except when used from within the type 
+
+
+            // Inside a class, you can invoke the delegate
+            MyEvent += (s) => { Debug.WriteLine("My Event is invoked from inside!"); };
+            MyEvent.Invoke("");
+        }
+    }
+}
