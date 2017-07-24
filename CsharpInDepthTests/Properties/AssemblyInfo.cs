10fe5d7b810e6cbdbaef7702ae557dd31d2bf2bd
Zanqi Liang <Zanqi_Liang@epam.com>
2017-07-24 15:16:50 +0800

    Add project files.

---
+++ CsharpInDepthTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CsharpInDepthTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("CsharpInDepthTests")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("12cd1663-4992-4b4c-95a7-a192d1246101")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
