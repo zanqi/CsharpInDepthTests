5bf6f6c8213352c799886b7ff1ba37c54a1ae09b
Zanqi <zanqi.liang@gmail.com>
2018-02-13 16:04:59 +0800

    Async exception handling.

---
+++ CsharpInDepthTests/AsyncTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CsharpInDepthTests
+{
+    [TestClass]
+    public class AsyncTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public async Task AwaitOnlyThrowFirstException()
+        {
+            Task task1 = Task.Run(() => { throw new ArgumentNullException("Message 1"); });
+            Task task2 = Task.Run(() => { throw new InvalidOperationException("Message 2"); });
+            await Task.WhenAll(task1, task2);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(AggregateException))]
+        public async Task CatchingMultipleExceptions()
+        {
+            Task task1 = Task.Run(() => { throw new ArgumentNullException("Message 1"); });
+            Task task2 = Task.Run(() => { throw new InvalidOperationException("Message 2"); });
+            await Task.WhenAll(task1, task2).WithAggregatedExceptions();
+        }
+    }
+}
---
+++ CsharpInDepthTests/TaskExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpInDepthTests
+{
+    static class TaskExtensions
+    {
+        public static AggregatedExceptionAwaitable WithAggregatedExceptions(this Task task)
+        {
+            return new AggregatedExceptionAwaitable(task);
+        }
+    }
+
+    internal class AggregatedExceptionAwaitable
+    {
+        private Task task;
+
+        public AggregatedExceptionAwaitable(Task task)
+        {
+            this.task = task;
+        }
+
+        public AggregatedExceptionAwaiter GetAwaiter()
+        {
+            return new AggregatedExceptionAwaiter(task);
+        }
+
+    }
+
+    internal class AggregatedExceptionAwaiter : INotifyCompletion
+    {
+        private Task task;
+
+        public AggregatedExceptionAwaiter(Task task)
+        {
+            this.task = task;
+        }
+
+        public bool IsCompleted
+        {
+            get { return task.GetAwaiter().IsCompleted; }
+        }
+
+        public void OnCompleted(Action continuation)
+        {
+            task.GetAwaiter().OnCompleted(continuation);
+        }
+
+        public void GetResult()
+        {
+            task.Wait(); // This is where multiple exceptions got wrapped
+        }
+    }
+}
