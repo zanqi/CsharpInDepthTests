fb62dd5f2a97cbe88f6cd375abfb70e888239f94
Zanqi <zanqi.liang@gmail.com>
2018-02-15 11:47:22 +0800

    Cancel task with token

--- CsharpInDepthTests/AsyncTests.cs
+++ CsharpInDepthTests/AsyncTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -83,7 +84,7 @@ namespace CsharpInDepthTests
         }
 
         [TestMethod]
-        public void CancelTask()
+        public void CancelTaskWithException()
         {
             Task task1 = ThrowCancellationException();
             Task task2 = ThrowOtherException();
@@ -102,6 +103,47 @@ namespace CsharpInDepthTests
         {
             throw new OperationCanceledException();
             await Task.Delay(100);
+        }
+
+        [TestMethod]
+        public void CancelTaskWithToken()
+        {
+            var source = new CancellationTokenSource();
+            var task = DelayFor30Seconds(source.Token);
+            source.CancelAfter(TimeSpan.FromSeconds(1));
+            Assert.AreEqual(task.Status, TaskStatus.WaitingForActivation);
+            try
+            {
+                task.Wait();
+            }
+            catch (AggregateException e)
+            {
+                Assert.IsInstanceOfType(e.InnerException, typeof(TaskCanceledException));
+            }
+            Assert.AreEqual(task.Status, TaskStatus.Canceled);
+        }
+
+        [TestMethod]
+        public async Task CancelTaskWithToken1()
+        {
+            var source = new CancellationTokenSource();
+            var task = DelayFor30Seconds(source.Token);
+            source.CancelAfter(TimeSpan.FromSeconds(1));
+            Assert.AreEqual(task.Status, TaskStatus.WaitingForActivation);
+            try
+            {
+                await task;
+            }
+            catch (TaskCanceledException e)
+            {
+
+            }
+            Assert.AreEqual(task.Status, TaskStatus.Canceled);
+        }
+
+        private async Task DelayFor30Seconds(CancellationToken token)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(30), token);
         }
     }
 }
